3032870082bc732e428bab013a762c46d6961da2
doswald32 <dan.oswald32@gmail.com>
2024-07-03 20:54:05 -0600

    seed info populated but need to correct id info in Books

--- lib/seed.py
+++ lib/seed.py
@@ -7,3 +7,11 @@ def seed_database():
     Patron.create_table()
     Book.drop_table()
     Book.create_table()
+
+    Steve_oswald = Patron.create("Steve", "Oswald", 62)
+    Carol_oswald = Patron.create("Carol", "Oswald", 62)
+    Book.create("Harry Potter 1", "JK Rowling", 305, "A young wizard confronts his dark past.", Steve_oswald.id)
+    Book.create("Harry Potter 2", "JK Rowling", 287, "The story continues.", Steve_oswald.id)
+    Book.create("The Tipping Point", "Malcom Gladwell", 256, "What makes a marketing campaign successful?", Carol_oswald.id)
+    Book.create("The DaVinci Code", "Dan Brown", 500, "It's a good one!", Carol_oswald.id)
+
